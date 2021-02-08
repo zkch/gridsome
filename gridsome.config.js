cf27e9acf32a513746ecc461fc72e1e674080d67
zhengkuncheng <zhengkuncheng@nome.com>
2021-02-08 14:12:34 +0800

    commit

---
+++ gridsome.config.js
@@ -0,0 +1,46 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: "Portfolio",
+  siteUrl: `http://117.50.93.22:8080`,
+  host: "0.0.0.0",
+
+  titleTemplate: "%s - NWA",
+  siteDescription: "Creative technologist",
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "projects/**/*.md",
+        typeName: "ProjectPost",
+        resolveAbsolutePaths: true,
+        remark: {
+          externalLinksTarget: "_blank",
+          externalLinksRel: ["nofollow", "noopener", "noreferrer"]
+        }
+      }
+    },
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "journal/**/*.md",
+        typeName: "JournalPost",
+        resolveAbsolutePaths: true,
+        remark: {
+          externalLinksTarget: "_blank",
+          externalLinksRel: ["nofollow", "noopener", "noreferrer"]
+        }
+      }
+    }
+  ],
+  transformers: {
+    remark: {
+      plugins: ["@gridsome/remark-prismjs"]
+    }
+  }
+};
+
